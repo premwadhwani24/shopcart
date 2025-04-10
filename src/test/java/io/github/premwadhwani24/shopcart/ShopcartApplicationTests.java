ea60820b1ae81bf7bfac745916eff6991a5677fe
Toshiba x360 <premwadhwani24@gmail.com>
2025-04-10 09:52:09 +0530

    initial commit

---
+++ src/test/java/io/github/premwadhwani24/shopcart/ShopcartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.premwadhwani24.shopcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
