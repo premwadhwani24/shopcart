bc7db893867870e138100b087f6952809cd0753a
premwadhwani24 <premwadhwani24@gmail.com>
2025-04-10 15:54:30 +0530

    Task CRUD

--- src/main/java/io/github/premwadhwani24/product/ProductController.java
+++ src/main/java/io/github/premwadhwani24/product/ProductController.java
@@ -1,37 +1,56 @@
 package io.github.premwadhwani24.product;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//REST= Representational State Tranfer (use for transfer data)
+import java.util.List;
+
+//REST= Representational State Transfer (use for transfer data)
 //JSON=JavaScript Object Notation
 @RestController
-public class ProductController() {
-    private ProductService productService;
+public class ProductController {
+    private final ProductService productService;
 
     @Autowired
     public ProductController(ProductService productService) {
-    this.productSerive=productService;
+        this.productService = productService;
     }
+
     //CREATE - POST
-    @PostConstruct("/products/add")
-    public Product addProduct(@RequestBody Product product){
+    @PostMapping("/products/add")
+    public Product addProduct(@RequestBody Product product) {
         productService.addProduct(product);
         return product;
     }
 
     //READ - GET
+    @GetMapping("/products")
+    public List<Product> getProductList() {
+        return productService.getProductList();
+    }
+
+    @GetMapping("product/{id}")
+    public Product getProduct(@PathVariable long id) {
+        return productService.getProduct(id);
+
+    }
 
     //UPDATE - PUT
+    @PutMapping("/products")
+    public Product updatePrice(Product product) {
+        return productService.updatePrice(product);
+    }
 
     //DELETE - DELETE
+    @DeleteMapping("/products")
+    public void deleteProduct(@RequestBody Product product) {
+        productService.deleteProduct(product);
+    }
+
 
     //REQUEST - HTTP METHOD & URL
 
     //RESPONSE - HTTP STATUS & RESPONSE BODY
-
 
 
 }
--- src/main/java/io/github/premwadhwani24/product/ProductService.java
+++ src/main/java/io/github/premwadhwani24/product/ProductService.java
@@ -1,6 +1,7 @@
 package io.github.premwadhwani24.product;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -21,8 +22,38 @@ public class ProductService {
         return product;
     }
     //READ
+    public List<Product> getProductList() {
+        return productList;
+    }
+    // READ -Single product
+    public Product getProduct(long id){
+        return productList.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+             }
+        }
 
     //UPDATE
+    public Product updatePrice(Product product) {
+        productList.stream()
+                .filter(p ->
+                        p.getID() == product.getId()
+                )
+                .findFirst()
+                .ifPresent(p ->
+                        p.setPrice(
+                                product.getPrice()
+                        )
+                );
+        return product;
+    }
 
-    //DELETE
+
+//DELETE
+public boolean deleteProduct(Product product){
+    productList.remove(product);
+    return true;
 }
+
