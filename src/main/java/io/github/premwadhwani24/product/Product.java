ff46be749d21a7c9f11d27b13805448192624c8d
premwadhwani24 <premwadhwani24@gmail.com>
2025-04-10 11:28:52 +0530

    new change

--- src/main/java/io/github/premwadhwani24/product/Product.java
+++ src/main/java/io/github/premwadhwani24/product/Product.java
@@ -1,4 +1,4 @@
-package io.github.premwadhwani24.shopcart;
+package io.github.premwadhwani24.product;
 
 import java.util.Objects;
 
---
+++ src/main/java/io/github/premwadhwani24/product/ProductService.java
@@ -0,0 +1,26 @@
+package io.github.premwadhwani24.product;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> productList;
+
+    public ProductService() {
+        productList = new LinkedList<>();
+    }
+
+    //CRUD - CREATE ,READ,UPDATE DELETE
+    //CREATE
+    public Product addProduct(Product product){
+        productList.add(product);
+        return product;
+    }
+    //READ
+    //UPDATE
+    //DELETE
+}
